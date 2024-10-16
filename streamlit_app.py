764dfe4b403ce2b4e6c5177ac0c6d0a2c90f4f2d
TheDutchSpartan <59475324+TheDutchSpartan@users.noreply.github.com>
2024-10-16 16:41:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit as st
 # from steamlit_gallery import apps, componetns
 # from streamlit_gallery import page_group
 
-with st.expander('Onderdelen',True):
-    page.item('Introductie')
-    page.item('Aannames')
-    page.item('Informatie terrein')
-    page.item('Energiebehoefte')
-    page.item('Conclusie/Aanbevelingen')
+st.sidebar.radio('Onderdelen',
+                 ["Introductie", "Aannames", "Informatie terrein","Energienehoefte","Conclusie/Aanbevelingen"],
+   
