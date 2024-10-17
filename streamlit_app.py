07fca832e5b0290d0bd7607342087e7c0ee1ddf6
TheDutchSpartan <59475324+TheDutchSpartan@users.noreply.github.com>
2024-10-17 19:32:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -175,30 +175,30 @@ elif blog_post == 'Informatie terrein':
     #                               tooltip=tooltip).add_to(m)
     #         except: "done"
 
-          def marker_toevoegen(adres, popup_adres, popup_sector, tooltip):
-            geolocator = Nominatim(user_agent="mijn_applicatie")
-            try:
-                locatie = geolocator.geocode(adres)
-                 if locatie:
+        def marker_toevoegen(adres, popup_adres, popup_sector, tooltip):
+          geolocator = Nominatim(user_agent="mijn_applicatie")
+          try:
+              locatie = geolocator.geocode(adres)
+                if locatie:
                      # HTML layout voor de popup
-                    html = f"""
-                    <div style="width:300px;">
-                    <table style="width:100%;">
-                    <tr><th>Adres:</th><td>{popup_adres}</td></tr>
-                     <tr><th>Sector:</th><td>{popup_sector}</td></tr>
-                     </table>
-                     </div>
-                     """
-                     popup = folium.Popup(html, max_width=300)
+                  html = f"""
+                   <div style="width:300px;">
+                   <table style="width:100%;">
+                   <tr><th>Adres:</th><td>{popup_adres}</td></tr>
+                    <tr><th>Sector:</th><td>{popup_sector}</td></tr>
+                   </table>
+                   </div>
+                    """
+                   popup = folium.Popup(html, max_width=300)
                 
-                    # Marker toevoegen met aangepaste popup
-                    folium.Marker(location=[locatie.latitude, locatie.longitude],
-                                   popup=popup,
-                                   tooltip=tooltip).add_to(m)
-                else:
-                      print(f"Adres niet gevonden: {adres}")
-            except Exception as e:
-                print(f"Fout bij geocoderen van {adres}: {e}")
+                   # Marker toevoegen met aangepaste popup
+                   folium.Marker(location=[locatie.latitude, locatie.longitude],
+                                 popup=popup,
+                                 tooltip=tooltip).add_to(m)
+               else:
+                    print(f"Adres niet gevonden: {adres}")
+          except Exception as e:
+               print(f"Fout bij geocoderen van {adres}: {e}")
 
 # Maak de map
 m = folium.Map(location=[52.395724, 4.789207], zoom_start=15, map = "OpenStreetMap")
