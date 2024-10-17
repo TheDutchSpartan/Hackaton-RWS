453b9157ba8ef7c09bc66af82926d3f79ae8a655
TheDutchSpartan <59475324+TheDutchSpartan@users.noreply.github.com>
2024-10-17 19:14:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -150,7 +150,7 @@ elif blog_post == 'Informatie terrein':
         # Toon de map
         st_folium(m, width=700, height=500)
 
-    # elif bedrijventerrein == 'Amsterdam Poort Noord':
+    elif bedrijventerrein == 'Amsterdam Poort Noord':
     #     # Functie om markers toe te voegen
     #     def marker_toevoegen(adres, popup_adres, popup_sector, tooltip):
     #         geolocator = Nominatim(user_agent="mijn_applicatie")
@@ -174,30 +174,30 @@ elif blog_post == 'Informatie terrein':
     #                               tooltip=tooltip).add_to(m)
     #         except: "done"
 
-def marker_toevoegen(adres, popup_adres, popup_sector, tooltip):
-            geolocator = Nominatim(user_agent="mijn_applicatie")
-            try:
-                locatie = geolocator.geocode(adres)
-                if locatie:
-                    # HTML layout voor de popup
-                    html = f"""
-                    <div style="width:300px;">
-                    <table style="width:100%;">
-                    <tr><th>Adres:</th><td>{popup_adres}</td></tr>
-                    <tr><th>Sector:</th><td>{popup_sector}</td></tr>
-                    </table>
-                    </div>
-                    """
-                    popup = folium.Popup(html, max_width=300)
+          def marker_toevoegen(adres, popup_adres, popup_sector, tooltip):
+              geolocator = Nominatim(user_agent="mijn_applicatie")
+              try:
+                  locatie = geolocator.geocode(adres)
+                  if locatie:
+                      # HTML layout voor de popup
+                      html = f"""
+                      <div style="width:300px;">
+                      <table style="width:100%;">
+                      <tr><th>Adres:</th><td>{popup_adres}</td></tr>
+                      <tr><th>Sector:</th><td>{popup_sector}</td></tr>
+                      </table>
+                      </div>
+                      """
+                      popup = folium.Popup(html, max_width=300)
                 
-                    # Marker toevoegen met aangepaste popup
-                    folium.Marker(location=[locatie.latitude, locatie.longitude],
-                                  popup=popup,
-                                  tooltip=tooltip).add_to(m)
-                else:
-                    print(f"Adres niet gevonden: {adres}")
-            except Exception as e:
-                print(f"Fout bij geocoderen van {adres}: {e}")
+                      # Marker toevoegen met aangepaste popup
+                      folium.Marker(location=[locatie.latitude, locatie.longitude],
+                                    popup=popup,
+                                    tooltip=tooltip).add_to(m)
+                  else:
+                      print(f"Adres niet gevonden: {adres}")
+              except Exception as e:
+                  print(f"Fout bij geocoderen van {adres}: {e}")
 
 # Maak de map
 m = folium.Map(location=[52.395724, 4.789207], zoom_start=15, map = "OpenStreetMap")
@@ -218,8 +218,9 @@ dfp_data.dropna(axis=0, how='all', inplace=True)
 dfp_data = dfp_data.drop(dfp_data.index[-1])
 for index, row in APN_data.iterrows():
     marker_toevoegen(row['Adres'], row["Adres"], row["Sector"], row["Bedrijfsnaam"])
-# Toon de map
 
+# Toon de map
+ st_folium(m, width=700, height=500)
   
 
 # ======================================================================================================================================================================
