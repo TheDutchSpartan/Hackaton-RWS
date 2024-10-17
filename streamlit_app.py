bb09b4a09bf3adf5c3c038815716da92603190a5
TheDutchSpartan <59475324+TheDutchSpartan@users.noreply.github.com>
2024-10-17 19:14:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -220,7 +220,7 @@ for index, row in APN_data.iterrows():
     marker_toevoegen(row['Adres'], row["Adres"], row["Sector"], row["Bedrijfsnaam"])
 
 # Toon de map
- st_folium(m, width=700, height=500)
+st_folium(m, width=700, height=500)
   
 
 # ======================================================================================================================================================================
